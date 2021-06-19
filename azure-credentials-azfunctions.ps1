9cabc82a89759d3d1d1e582557fe160b78320183
Renato Groffe <renato.groffe@yahoo.com.br>
2021-06-19 15:47:32 -0300

    GitHub Actions + .NET 5 + Azure Functions + Azure CLI
    GitHub Actions + .NET 5 + Azure Functions + Azure CLI

---
+++ azure-credentials-azfunctions.ps1
@@ -0,0 +1,1 @@
+az ad sp create-for-rbac --name "GroffeGitHubActionsAzureFunctions" --role contributor --scopes /subscriptions/<SUBSCRIPTION_ID>/resourceGroups/<RESOURCE_GROUP>/providers/Microsoft.Web/sites/<FUNCTION APP> --sdk-auth
